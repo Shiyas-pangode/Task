6353f8fb5b1b039c20a49a087b356de1f9148893
shiyas-pangode <shiyasschachu@gmail.com>
2025-03-20 15:14:34 +0530

    update

--- blogapi/v1/postapi/urls.py
+++ blogapi/v1/postapi/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 urlpatterns = [
 
-    path('post/', PostCreatedView.as_view(), name = 'create_post'),
+    path('post/', PostCreatedView.as_view(), name = 'create_post'), #postcreation
+    path('post/posts/',PostListView.as_view(), name='post_list_search'), #post list view
     path('post/<int:pk>/', PostRetrieveView.as_view(), name = 'retrieve_single_view'),
-    path('posts/',PostListView.as_view(), name='post_list_search'),
-    path('post/<int:pk>/author/',BlogDetailView.as_view(),name = 'postDeleteUpdate'),
+    path('post//posts/?q=title' ,PostListView.as_view(), name='post_list_search'), #post search based on title
+    path('post/<int:pk>/author/',BlogDetailView.as_view(),name = 'postDeleteUpdate'), #post update and delete filterd by author
 
 ]
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('auth/', include('blogapi.v1.auth.urls')),
-    path('user/', include('blogapi.v1.users.urls')),
+    path('auth/', include('blogapi.v1.SigninAPI.urls')),
+    path('user/', include('blogapi.v1.SignupAPI.urls')),
     path('postapi/', include('blogapi.v1.postapi.urls')),
     path('resetapi/', include('blogapi.v1.password.urls')),
 ]
